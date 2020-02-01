2970834937f90ee9cc4ccceacd7e49e4d93b4aec
Hosung <runway3207@hanmail.net>
2020-02-01 02:12:19 -0500

    finally working!!

--- App.js
+++ App.js
@@ -1,50 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { Camera } from "expo-camera";
+import {Camera} from "expo-camera";
 
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
+export default class App extends Component {
+    state = {
+      permission: null,
+      camera: null
+  }
+  
+  componentDidMount = async () => {
+      const perm = await Camera.requestPermissionsAsync();
+      console.log(perm.status);
+      this.setState({permission: (perm.status == 'granted')});
+      console.log(this.state.permission);
+  }
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === "granted");
-    })();
-  }, []);
-
-  take = async () => {
+  take = async (cam) => {
+    console.log(cam);
     const options = { quality: 1, base64: true, exif: true };
     console.log("trying taking picture");
 
-    await Camera.takePictureAsync(options).then(result => {
-      console.log(result.width);
-    });
+    var test =await cam.takePictureAsync(options);
+    console.log(test);
   };
-
-  if (hasPermission === null) {
-    return <View />;
+  render() {
+    if (this.state.permission == null) {
+      return <View />;
+    }
+    if (this.state.permission == false) {
+      return <Text>No access to camera</Text>;
+    }
+    return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} ratio="16:9" ref={(ref)=>{this.camera=ref}}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: "transparent",
+                flexDirection: "row"
+              }}
+            ></View>
+            <TouchableOpacity style={styles.takeButton} onPress={()=>this.take(this.camera)}>
+              <Text>Take</Text>
+            </TouchableOpacity>
+          </Camera>
+        </View>
+      );
+    }
   }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-  return (
-    <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type} ratio="16:9">
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: "transparent",
-            flexDirection: "row"
-          }}
-        ></View>
-        <TouchableOpacity style={styles.takeButton} onPress={take}>
-          <Text>Take</Text>
-        </TouchableOpacity>
-      </Camera>
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   takeButton: {
