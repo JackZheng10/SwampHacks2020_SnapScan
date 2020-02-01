1435ddd77e969b415eb98ecb5d7833352d93f4a2
Jack Zheng <jackzheng10@yahoo.com>
2020-02-01 17:48:36 -0500

    testing scroll

--- components/ScanInfoScreen.js
+++ components/ScanInfoScreen.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import { Text, View, SafeAreaView, TextInput } from 'react-native';
-import {styles} from './styles.js'
+import React from "react";
+import { Text, View, SafeAreaView, TextInput, ScrollView } from "react-native";
+import { styles } from "./styles.js";
+import { Header } from "react-native-elements";
 
-  class Test extends React.Component {
-
-   render() {
+class Test extends React.Component {
+  render() {
     return (
-        <View key="2" style={styles.slideBody}>
-            <Text>something</Text>
-        </View>
+      <SafeAreaView>
+        <Header
+          placement="center"
+          centerComponent={{
+            text: "Scan Results",
+            style: styles.scanInfoText
+          }}
+          containerStyle={styles.scanInfoHeader}
+        />
+        <ScrollView>
+          <Text>AHSHDASHDASDISAIAIAHSHDASDISADISAIAIAHSHDASDISAIAI</Text>
+        </ScrollView>
+      </SafeAreaView>
     );
-    }
+  }
 }
 
- export default Test;
+export default Test;
