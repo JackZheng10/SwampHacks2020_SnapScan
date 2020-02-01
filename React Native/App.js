350844f227b39a9becad1c35df8ec6dd23075ade
Hosung <runway3207@hanmail.net>
2020-01-31 23:44:58 -0500

    expo camera

---
+++ React Native/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  take = async () => {
+    console.log("trying taking picture");
+    Camera.takePictureAsync({
+      quality: 1,
+      base64: true,
+      onPictureSaved: (uri, width, height, exif, base64)=>{
+        console.log(uri);
+      }
+    })
+  }
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} type={type}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}>
+          
+        </View>
+        <TouchableOpacity style={styles.takeButton} onPressout={take()}>
+            <Text>Take</Text>
+          </TouchableOpacity>
+      </Camera>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  takeButton: {
+    width: 100,
+    alignSelf: "center",
+    marginBottom: 20
+  }
+});
